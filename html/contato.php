6ffd75a4042d37d52f1a0f047da261a914b2b097
Gabriel Rezende <rezende.gabriel@engenharia.ufjf.br>
2017-10-28 02:14:31 -0200

    todos os includes realidos, atualizacao do bootstrap e organizacao do diretorio

---
+++ html/contato.php
@@ -0,0 +1,25 @@
+		<section id="contato" class="container">
+			<h2>Contato</h2>
+			<p>Entre em contato conosco.</p>
+			<form>
+				<div class="form-group">
+					<label for="contato-name">Nome:</label>
+					<input class="form-control" id="contato-name" type="text" placeholder="Seu nome">
+				</div>
+				<div class="form-group">
+					<label for="contato-email">Email:</label>
+					<div class="input-group">
+						<div class="input-group-addon">@</div>
+						<input class="form-control" id="contato-email" type="email" placeholder="Seu e-mail">
+					</div>
+				</div>
+				<div class="form-group">
+    				<label for="contato-mensagem">Mensagem:</label>
+    				<textarea class="form-control" id="contato-mensagem" rows="9"></textarea>
+  				</div>
+			</form>
+			<button class="btn btn-default" type="submit">Enviar</button>
+		</section>
+		<script src="../assets/js/jquery.js"></script>
+		<script src="../assets/js/bootstrap.min.js"></script>
+	</body>
--- html/header.php
+++ html/header.php
@@ -1,4 +1,3 @@
-
 		<header>
 			<nav class="navbar navbar-inverse" style="background-color: #36454f">
 				<div class="container-fluid">
@@ -9,31 +8,29 @@
 					        <span class="icon-bar"></span>
 					        <span class="icon-bar"></span>
 					    </button>
-    					<a class="navbar-brand" href="#">JF Alarmes</a>
-    				</div>
-	    			<div class="collapse navbar-collapse" id="navbar">
-	    				<ul class="nav navbar-nav">
-	    					<li><a href="#">Home</a></li>
-	    					<li><a href="#">Quem somos</a></li>
-	    					<li><a href="#">Produtos</a></li>
-	    					<li><a href="#contato">Contato</a></li>
-	    					<li><a href="#">Localização</a></li>
+						<a class="navbar-brand" href="#">JF Alarmes</a>
+					</div>
+					<div class="collapse navbar-collapse" id="navbar">
+						<ul class="nav navbar-nav">
+							<li><a href="#">Home</a></li>
+							<li><a href="#quemsomos">Quem somos</a></li>
+							<li><a href="#">Produtos</a></li>
+							<li><a href="#contato">Contato</a></li>
+							<li><a href="#localização">Localização</a></li>
 		    				<li class="dropdown">
 		    					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span><span class="caret"></span></a>
 		    					<ul class="dropdown-menu">
-		    						<div class="container">
-			    						<form>
-			    							<div class="form-group">
-			    								<label for="login">Login:</label>
-			    								<input class="form-control" id="login" type="text" placeholder="Usuário">
-			    							</div>
-			    							<div class="form-group">
-			    								<label for="login">Senha:</label>
-			    								<input class="form-control" id="login" type="text" placeholder="Sua senha">
-			    							</div>
-			    						</form>
-			    						<button class="btn btn-default" type="submit">Entrar</button>
-			    					</div>	
+		    						<form>
+		    							<div class="form-group">
+		    								<label for="login">Login:</label>
+		    								<input class="form-control" id="login" type="text" placeholder="Usuário">
+		    							</div>
+		    							<div class="form-group">
+		    								<label for="login">Senha:</label>
+		    								<input class="form-control" id="login" type="text" placeholder="Sua senha">
+		    							</div>
+		    						</form>
+		    						<button class="btn btn-default" type="submit">Entrar</button>
 		    					</ul>
 		    				</li>
 		    			</ul>
@@ -43,7 +40,7 @@
 									<input type="text" class="form-control" placeholder="Pesquisa">
 									<span class="input-group-btn">
 										<button type="button" class="btn btn-default" aria-label="Left Align">
-									 		<span class=" glyphicon glyphicon-search" aria-hidden="true"></span>
+									 		<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
 										</button>
 									</span>
 								</div>
