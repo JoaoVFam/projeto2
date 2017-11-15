c364775e1ed74972e29bebc8c6cd9d9c18ffb71f
Eleno Trindade <eleno.trindade@hotmail.com>
2017-11-14 22:12:02 -0200

    slide 100% responsivo e funcional, até que enfim

--- html/home.php
+++ html/home.php
@@ -1,6 +1,6 @@
 		<section id="home">
 			<div>
-				<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<div id="myCarousel" class="carousel slide hidden-xs" data-ride="carousel">
 					<ol class="carousel-indicators">
 						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
 						<li data-target="#myCarousel" data-slide-to="1"></li>
@@ -8,22 +8,22 @@
 					</ol>
 					<div class="carousel-inner">
 						<div class="item active">
-							<img src="../assets/img/slide11.jpg" alt="slide1" style="width:100%" class="img-slide" />
-							<div class="carousel-caption img-slide">
+							<img src="../assets/img/slide11.jpg" alt="slide1" style="width:100%" class="img-responsive" />
+							<div class="carousel-caption">
 								<h4>Alarmes residenciais e comerciais</h4>
 								<p>Nossos sistemas de alarmes permitem que seu patrimônio fique protegido no momento de ociosidade do imóvel.</p>
 							</div>
 							</div>
 						    <div class="item">
-						        <img src="../assets/img/slide21.jpg" alt="slide2" style="width:100%" class="img-slide"/>
-							    <div class="carousel-caption img-slide">
+						        <img src="../assets/img/slide21.jpg" alt="slide2" style="width:100%" class="img-responsive"/>
+							    <div class="carousel-caption">
 							        <h4>Automação de Portões</h4>
 							        <p>A JF Alarmes também presta serviços de Automação de Portões que proporcionam conforto para a família.</p>
 							    </div>
 						    </div>
 							<div class="item">
-							    <img src="../assets/img/slide31.jpg" alt="slide3" style="width:100%" class="img-slide"/>
-								<div class="carousel-caption img-slide">
+							    <img src="../assets/img/slide31.jpg" alt="slide3" style="width:100%" class="img-responsive"/>
+								<div class="carousel-caption">
 									<h4>Cerca Elétrica</h4>
 									<p>Inibe a ação de invasores através de descarga elétrica.</p>
 								</div>
@@ -34,6 +34,45 @@
 						    <span class="sr-only">Previous</span>
 					    </a>
 					    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+						    <span class="glyphicon glyphicon-chevron-right"></span>
+						    <span class="sr-only">Next</span>
+				  		</a>
+				</div>
+<!-- PARTE PARA CELULAR USANDO AS CLASSES DE HIDE -->
+				<div id="myCarousel2" class="carousel slide visible-xs-block" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#myCarousel2" data-slide-to="0" class="active"></li>
+						<li data-target="#myCarousel2" data-slide-to="1"></li>
+						<li data-target="#myCarousel2" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="item active">
+							<img src="../assets/img/slide1.jpg" alt="slide1" style="width:100%" class="img-responsive" />
+							<div class="carousel-caption">
+								<h4>Alarmes residenciais e comerciais</h4>
+								<p>Nossos sistemas de alarmes permitem que seu patrimônio fique protegido no momento de ociosidade do imóvel.</p>
+							</div>
+							</div>
+						    <div class="item">
+						        <img src="../assets/img/slide2.jpg" alt="slide2" style="width:100%" class="img-responsive"/>
+							    <div class="carousel-caption">
+							        <h4>Automação de Portões</h4>
+							        <p>A JF Alarmes também presta serviços de Automação de Portões que proporcionam conforto para a família.</p>
+							    </div>
+						    </div>
+							<div class="item">
+							    <img src="../assets/img/slide3.jpg" alt="slide3" style="width:100%" class="img-responsive"/>
+								<div class="carousel-caption">
+									<h4>Cerca Elétrica</h4>
+									<p>Inibe a ação de invasores através de descarga elétrica.</p>
+								</div>
+						    </div>
+					    </div>
+					    <a class="left carousel-control" href="#myCarousel2" data-slide="prev">
+						    <span class="glyphicon glyphicon-chevron-left"></span>
+						    <span class="sr-only">Previous</span>
+					    </a>
+					    <a class="right carousel-control" href="#myCarousel2" data-slide="next">
 						    <span class="glyphicon glyphicon-chevron-right"></span>
 						    <span class="sr-only">Next</span>
 				  		</a>
