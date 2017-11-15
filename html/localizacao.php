9f3259b8fa65b5b68f068e7c0f6faeefe9c5a9bd
Eleno Trindade <eleno.trindade@hotmail.com>
2017-11-14 22:36:08 -0200

    pagina localizacao com o tamanho atualizado

--- html/localizacao.php
+++ html/localizacao.php
@@ -14,8 +14,14 @@
 					</div>
 					<div class="col-md-6">
 						<div class="embed-responsive embed-responsive-4by3 mapa-desktop">
-						<iframe class="thumbnail" class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3705.678383497171!2d-43.359949849704556!3d-21.7539907731984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x989c84b391dee7%3A0x7075ddb84b86a9cb!2sJF+Alarmes+-+Sistemas+Eletr%C3%B4nicos+de+Seguran%C3%A7a!5e0!3m2!1spt-BR!2sbr!4v1507133905205" allowfullscreen></iframe>
+							<iframe class="thumbnail" class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3705.678383497171!2d-43.359949849704556!3d-21.7539907731984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x989c84b391dee7%3A0x7075ddb84b86a9cb!2sJF+Alarmes+-+Sistemas+Eletr%C3%B4nicos+de+Seguran%C3%A7a!5e0!3m2!1spt-BR!2sbr!4v1507133905205" allowfullscreen></iframe>
+						</div>
 					</div>
 				</div>
-			</div>
+				<div>
+					<div class="embed-responsive embed-responsive-4by3 mapa-desktop" style="padding: 0px -15px">
+						<iframe class="thumbnail" class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m0!4v1510705264759!6m8!1m7!1stg9M8WZNmS3xACUQ3ektPQ!2m2!1d-21.75403067498537!2d-43.35871803481814!3f144.31648769017272!4f4.881573776345348!5f0.7820865974627469" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>	
 		</section>
