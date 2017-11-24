5cb51815457523398bae2ecc65b36f16cbee1db7
Eleno Trindade <eleno.trindade@hotmail.com>
2017-11-24 03:14:57 -0200

    ...

--- html/header.php
+++ html/header.php
@@ -14,10 +14,10 @@
 	</head>
 	<body style="padding-top: 50px" id="home">
 		<header>
-			<nav class="navbar navbar-fixed-top navbar-inverse">
+			<nav class="navbar navbar-fixed-top navbar-inverse col-xs-12">
 				<div class="container-fluid">
 					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="true">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
 					        <span class="sr-only">Toggle navigation</span>
 					        <span class="icon-bar"></span>
 					        <span class="icon-bar"></span>
